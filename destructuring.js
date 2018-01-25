869e5ac2306fb4d6188d517db9213800d9f78180
Michael Koerber <michaelkoerber@michaels-mbp.attlocal.net>
2018-01-25 07:59:45 -0600

    finished classes and destructuring

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+let {color, make, model, year} = carDetails
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+let{firstName, lastName, title} = obj;  
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,9 +53,13 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
-
-
+var totalPopulation = (obj) => {
+  let states = {utah, california, texas, arizona} = obj;
+  let statesArr = [utah, california, texas, arizona];
+  return statesArr.reduce(function(a,b){
+    return a + b;
+  })
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -67,7 +71,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+function ingredients(obj){
+  let {carb, fat, protein} = obj;
+  let arr = [];
+  arr.push(carb, fat, protein);
+  return arr;
+}
 
 
 
@@ -85,7 +94,11 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers(obj){
+  let {first, second, third} = obj;
+  let numArr = [first, second, third];
+  return Math.min.apply(null, numArr);
+}
 
 
 
@@ -97,6 +110,15 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
-
-
+function numberGroups( obj ) {
+  let {a,b,c} = obj;
+  let groupArr = [a, b, c];
+  let longest = [];
+  for(let i=0;i<groupArr.length;i++){
+    if(groupArr[i].length > longest.length){
+      longest = [];
+      longest.push(...groupArr[i]);
+    }
+  }
+  return longest;
+}
